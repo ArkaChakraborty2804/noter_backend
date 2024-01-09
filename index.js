b879d6182f5e63475d586307504a4a3c011f7115
Arka Chakraborty <arkachakraborty@Arkas-MacBook-Air.local>
2024-01-10 00:30:20 +0530

    get /

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const app = express()
+
+app.get('/',(req,res)=>{
+    console.log('Hi there!')
+})
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server running on port ${process.env.PORT}`)
 })
