5cd8118282f9bdb99cb210395a62e0a0912fc8df
Arka Chakraborty <arkachakraborty@Arkas-MacBook-Air.local>
2024-01-10 01:47:22 +0530

    mongo uri modified

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser')
 
 const connectDB =async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL,{dbName:'test'})
         console.log("database is connected successfully!")
     }
     catch(err){
