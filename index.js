9943a53cdbd8fb54fecd64709afceb3847a4de3e
Arka Chakraborty <arkachakraborty@Arkas-MacBook-Air.local>
2024-01-10 01:18:05 +0530

    Basic routes added

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const app = express()
+const mongoose=require('mongoose')
+const authRoute = require('./routes/auth')
+const cookieParser = require('cookie-parser')
+
+const connectDB =async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("database is connected successfully!")
+    }
+    catch(err){
+        console.log(err)
+    }
+}
 
 dotenv.config()
-
-const app = express()
-
-app.get('/',(req,res)=>{
-    res.status(200).json({
-        msg: "Working"
-    })
-})
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRoute)
 
 app.listen(process.env.PORT,()=>{
+    connectDB()
     console.log(`Server running on port ${process.env.PORT}`)
 })
---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+//REGISTER
+router.post('/signup', async(req,res)=>{
+    try{
+        const {username, email, password} = req.body
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+        const newUser = new User({username, email, password:hashedPassword})
+        const savedUser = await newUser.save()
+        res.status(200).json(savedUser)
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json(err)
+    }
+})
+
+//LOGIN
+router.post('/login',async(req,res)=>{
+    try {
+        const user = User.findOne({email:req.body.email})
+        if(!user){
+            return res.status(404).json({msg:"User not found"})
+        }
+        const match = await bcrypt.compare(req.body.password, user.password)
+        if(!match){
+            return res.status(401).json({msg:"Password does not match"})
+        }
+        const token = jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:'3d'})
+        const {password, ...info} = user._doc
+        res.cookie('token',token).status(200).json(info)
+    } catch(err) {
+        console.error(err)
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
