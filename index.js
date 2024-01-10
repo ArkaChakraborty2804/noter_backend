45fbcd747115153ed0d705cd21ea815f581d5c64
Arka Chakraborty <arkachakraborty@Arkas-Air.mshome.net>
2024-01-10 15:38:39 +0530

    layout changed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const connectDB =async()=>{
 dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/auth",authRoute)
+// app.use("/api/auth",authRoute)
 app.use(cors({origin:'*' ,
  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", 
  preflightContinue: false , 
@@ -34,6 +34,42 @@ app.get("/", async(req,res)=> {
     res.setHeader('Access-Control-Allow-Credentials', true) ;    
     res.send("Hello World ");
 });
+
+//REGISTER
+app.post('/signup', async(req,res)=>{
+    try{
+        const {username, email, password} = req.body
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+        const newUser = new User({username, email, password:hashedPassword})
+        const savedUser = await newUser.save()
+        res.status(200).json(savedUser)
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json(err)
+    }
+})
+
+//LOGIN
+app.post('/login',async(req,res)=>{
+    try {
+        const user = User.findOne({email:req.body.email})
+        if(!user){
+            return res.status(404).json({msg:"User not found"})
+        }
+        const match = await bcrypt.compare(req.body.password, user.password)
+        if(!match){
+            return res.status(401).json({msg:"Password does not match"})
+        }
+        const token = jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:'3d'})
+        const {password, ...info} = user._doc
+        res.cookie('token',token).status(200).json(info)
+    } catch(err) {
+        console.error(err)
+        res.status(500).json(err)
+    }
+})
 
 app.listen(process.env.PORT,()=>{
     connectDB()
