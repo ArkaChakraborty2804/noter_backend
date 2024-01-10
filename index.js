f9e8e355d3a79fb9e3b693ff175fc4e480155573
Arka Chakraborty <arkachakraborty@Arkas-Air.mshome.net>
2024-01-10 15:24:54 +0530

    modification in cors

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ app.use(cors({origin:'*' ,
  optionsSuccessStatus: 204}));
 app.use(express.json());
 
+app.get("/", async(req,res)=> {
+    res.setHeader('Access-Control-Allow-Origin', '*')  ;
+    res.setHeader('Access-Controller-Allow-Methods','GET,POST,OPTIONS,PUT,PATCH,DELETE'); 
+    res.setHeader('Access-Controller-Allow-Headers','X-Requested-With,content-type');  
+    res.setHeader('Access-Control-Allow-Credentials', true) ;    
+    res.send("Hello World ");
+});
+
 app.listen(process.env.PORT,()=>{
     connectDB()
     console.log(`Server running on port ${process.env.PORT}`)
