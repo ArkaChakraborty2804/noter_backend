0ba38b1f626bca7381f59d2f4a79647eef0d7fcd
Arka Chakraborty <arkachakraborty@Arkas-Air.mshome.net>
2024-01-10 17:07:47 +0530

    cookie handled

--- index.js
+++ index.js
@@ -65,8 +65,7 @@ app.post('/login',async(req,res)=>{
             return res.status(401).json({msg:"Password does not match"})
         }
         const token = jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:'3d'})
-        const {password, ...info} = user._doc
-        res.cookie('token',token).status(200).json(info)
+        res.cookie('token',token).status(200).json({msg:"logged in"})
     } catch(err) {
         console.error(err)
         res.status(500).json(err)
